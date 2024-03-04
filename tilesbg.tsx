54f51a6193c18e50698c808be19b7faa17746230
mitchellconway <mitch@cottonwoodalc.org>
2024-03-04 15:32:30 -0700

    removed and attempt

--- tilesbg.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tilesbg" tilewidth="3606" tileheight="3840" tilecount="1" columns="1">
- <image source="../../../Downloads/Untitled design.png" width="3606" height="3840"/>
-</tileset>
