41ebb1abb3f2cfde29ec39c045e5ad8cc2c1c75c
mitchellconway <mitch@cottonwoodalc.org>
2024-03-04 13:04:41 -0700

    trying to adjust

---
+++ tiles (lower).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles (lower)" tilewidth="1500" tileheight="1500" tilecount="4" columns="2">
+ <image source="../../../Downloads/Untitled design.png" width="3606" height="3840"/>
+</tileset>
---
+++ tiles4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles4" tilewidth="3600" tileheight="3600" tilecount="1" columns="1">
+ <image source="../../../Downloads/Untitled design.png" width="3606" height="3840"/>
+</tileset>
