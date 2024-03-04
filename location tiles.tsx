553f8d020c0bec7dfd326e04e3c22f83e4e9b0e9
mitchellconway <mitch@cottonwoodalc.org>
2024-03-04 14:12:15 -0700

    fix background

---
+++ location tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="location tiles" tilewidth="3606" tileheight="3840" tilecount="1" columns="1">
+ <image source="tilesets/Untitled design.png" width="3606" height="3840"/>
+</tileset>
